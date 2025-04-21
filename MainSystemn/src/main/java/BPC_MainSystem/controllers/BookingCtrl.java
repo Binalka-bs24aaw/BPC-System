778c0f0eab3e9bbff6c86cba8ccc4fd10da940e8
Binalka Amarajeewa <bs24aaw@herts.ac.uk>
2025-04-21 23:26:04 +0100

    complete booking controller

---
+++ MainSystemn/src/main/java/BPC_MainSystem/controllers/BookingCtrl.java
@@ -0,0 +1,16 @@
+package BPC_MainSystem.controllers;
+
+public class BookingCtrl {
+    public BookingCtrl(MainSystemCtrl mainSystemCtrl) {
+    }
+
+    public static void startBooking() {
+    }
+
+    public static void cancelBooking() {
+    }
+
+    public static void attendBooking() {
+
+    }
+}
