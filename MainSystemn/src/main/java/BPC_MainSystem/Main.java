5a831dff8e44d4bc927da765105c4c08ada5f8b6
Binalka Amarajeewa <bs24aaw@herts.ac.uk>
2025-04-26 00:25:43 +0100

    update main java file

--- MainSystemn/src/main/java/BPC_MainSystem/Main.java
+++ MainSystemn/src/main/java/BPC_MainSystem/Main.java
@@ -1,11 +1,15 @@
 package BPC_MainSystem;
 
-
+import BPC_MainSystem.controllers.MainSystemCtrl;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        //System.out.println("Hello, World!");
+        MainSystemCtrl ctrl = new MainSystemCtrl();
+
+        ctrl.TopMessage();
+        ctrl.startMainSystem();
 
 
 
